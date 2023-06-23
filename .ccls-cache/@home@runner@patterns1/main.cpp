0b225aae1d798cd9bc576a3b149dc5e5cc2d87c9
skb31 <137195927+skb31@users.noreply.github.com>
2023-06-23 16:14:31 +0000

    final changes

--- .ccls-cache/@home@runner@patterns1/main.cpp
+++ .ccls-cache/@home@runner@patterns1/main.cpp
@@ -8,6 +8,7 @@ cout<<"Enter the number of rows"<<"\n";
 cin>>rows;
 cout<<"Enter the maximum number of columns: "<<"\n";
 cin>>columns;
+  
 for( int i=0;i<=rows;i++){
 for (int j=0;j<i;j++){
 cout<<"*";
